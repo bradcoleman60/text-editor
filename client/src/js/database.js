e6a17801edf59f00267d29523f1e01d58349a625
Brad Coleman <bradcoleman60@gmail.com>
2023-02-28 18:25:55 -0800

    adds indexedDB code to save data and get all data from DB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,51 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  console.log('Post to the database')
 
+  // Create a connection and version
+  const texteditorDb = await openDB('editor', 1);
+  
+  //Create a new transaction 
+  const tx = texteditorDb.transaction('editor', 'readwrite');
+
+  //Open up the desired object store
+  const store = tx.objectStore('editor');
+
+  //Use .add() method to store and pass content to database
+  const request = store.add({content});
+
+  //Get confirmation of request
+  const result = await request;
+  console.log( 'data saved to the editor database', result);
+
+
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  console.log('get all content from editor database');
 
+  // Create a connection and version
+  const texteditorDb = await openDB('editor', 1);
+  
+  //Create a new transaction 
+  const tx = texteditorDb.transaction('editor', 'readonly');
+
+  //Open up the desired object store
+  const store = tx.objectStore('editor');
+
+  // Use .getAll() method to get all data from editor database
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+
+
+
+}
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,57 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Webpack Plugin generates html and creates bundles
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text-Editor'
+      }),
+
+      // Inject custom service worker
+      new InjectManifest({
+        swSrc: './src-sq.js',
+        swDest: 'src-sw.js',
+      }),
+
+      // Creates a manifest.json file
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'Editor',
+        description: 'Edit your text',
+        theme: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          },
+        ],
+      })
       
     ],
 
     module: {
+      //CSS loaders
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            }
+          }
+        }
       ],
     },
   };
